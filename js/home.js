e52037d934f8c673b7fe20249bf937d4443512bb
Andrew Marvin <andrewtmarvin@gmail.com>
2021-06-25 16:38:46 -0600

    added about section content and animation

--- js/home.js
+++ js/home.js
@@ -1,1 +1,6 @@
-console.log('welcome to the homepage');
+document.querySelectorAll('.fade-in-text').forEach(textBlock => {
+    for (let i = 0; i < textBlock.children.length; i++) {
+        let node = textBlock.children[i];
+        setTimeout(function() { node.classList.add('fade-in-text-visible')}, (i + 1) * (i + 1) * 500)
+    }
+})
