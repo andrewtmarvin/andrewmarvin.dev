7a7449ecb56c22f34e5ee3839c750765ec0fa1ca
Andrew Marvin <andrewtmarvin@gmail.com>
2021-07-01 18:14:57 -0600

    added fade in element effect

--- js/home.js
+++ js/home.js
@@ -1,22 +1,16 @@
-// INTERSECTION OBSERVER SECTION
+// INTERSECTION OBSERVER
 
-// Fade in lines observer
-let timeOutIDs = [];
-let observer = new IntersectionObserver(function(textBlocks) {
+// Fade in lines
+let fadeInTextLinesObserver = new IntersectionObserver(function(textBlocks) {
     textBlocks.forEach(function (textBlock) {
         const {isIntersecting} = textBlock;
         if (isIntersecting === true) {
             for (let i = 0; i < textBlock.target.children.length; i++) {
                 let element = textBlock.target.children[i];
-                timeOutIDs.push(setTimeout(() => element.classList.add('fade-in-text-visible'), (i * 1250)));
+                setTimeout(() => element.classList.add('fade-in-text-lines-visible'), (i * 1250));
             }
-        } else {
-            for (let i = 0; i < textBlock.target.children.length; i++) {
-                let element = textBlock.target.children[i];
-                timeOutIDs.forEach(ID => clearInterval(ID));
-                element.classList.remove('fade-in-text-visible');
-            }
-        }
+            fadeInTextLinesObserver.unobserve(textBlock.target);
+        } 
     });
 },
 {
@@ -24,5 +18,25 @@ let observer = new IntersectionObserver(function(textBlocks) {
     threshold: 1
 });
 
-const fadeInTextElements = document.querySelectorAll('.fade-in-text');
-fadeInTextElements.forEach(element => observer.observe(element));
+const fadeInTextElements = document.querySelectorAll('.fade-in-text-lines');
+fadeInTextElements.forEach(element => fadeInTextLinesObserver.observe(element));
+
+// Fade in element
+let fadeInElementObserver = new IntersectionObserver(function(elements) {
+    elements.forEach(function (element) {
+        const {isIntersecting} = element;
+        if (isIntersecting === true) {
+            element.target.classList.add('fade-in-element-visible');
+            fadeInElementObserver.unobserve(element.target);
+        } 
+    });
+},
+{
+    rootMargin: '-20% 0% -20% 0%',
+    threshold: 0
+});
+
+const fadeInElements = document.querySelectorAll('.fade-in-element');
+fadeInElements.forEach(element => fadeInElementObserver.observe(element));
+
+// TERMINAL COMPONENT
